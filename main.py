89b9e1c75c6737d92ef59a48ebcf8eccd994b366
Daniel Kinsman <danielkinsman@riseup.net>
2023-09-27 15:29:48 +1000

    working with no error handling

--- main.py
+++ main.py
@@ -1,51 +1,25 @@
 #! /usr/bin/env python
 
 import asyncio
+import aiohttp
 import discord
 import discord.ext.tasks
 import logging
 import os
-import requests
-import requests.exceptions
 
 
 TWITCH_CLIENT_ID_ENV = "TWITCH_CLIENT_ID"
 TWITCH_CLIENT_SECRET_ENV = "TWITCH_CLIENT_SECRET"
 NETHACK_TWITCH_GAME_ID = 130
+TWITCH_QUERY = f"https://api.twitch.tv/helix/streams?game_id={NETHACK_TWITCH_GAME_ID}&type=live"
 #NETHACK_TWITCH_GAME_ID = 516575 # valorant for testing
 # get again with this:
 #   response = twitch.get("https://api.twitch.tv/helix/games?name=nethack&name=nethack-1987")
 
-DISCORD_CLIENT_ID_ENV = "DISCORD_CLIENT_ID"
-DISCORD_CLIENT_SECRET_ENV = "DISCORD_CLIENT_SECRET"
 DISCORD_BOT_TOKEN_ENV = "DISCORD_BOT_TOKEN"
-DISCORD_PERMISSIONS = 18432
 DISCORD_CHANNEL = 1156444485442617464
 
 POLL_TIME = 60
-ERROR_RETRY_TIME = 120
-
-
-def twitch_session():
-    client_id = os.environ[TWITCH_CLIENT_ID_ENV]
-    client_secret = os.environ[TWITCH_CLIENT_SECRET_ENV]
-    data = {
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "grant_type": "client_credentials",
-    }
-    response = requests.post("https://id.twitch.tv/oauth2/token", data=data)
-    response.raise_for_status()
-
-    token = response.json()['access_token']
-
-    session = requests.Session()
-    session.headers = {
-        "Authorization": f"Bearer {token}",
-        "Client-Id": client_id,
-    }
-
-    return session
 
 
 class DiscordClient(discord.Client):
@@ -55,62 +29,54 @@ class DiscordClient(discord.Client):
     async def on_ready(self):
         logging.info(f"Discord logged in as {self.user}")
 
+    async def twitch_auth(self):
+        self.twitch_client_id = os.environ[TWITCH_CLIENT_ID_ENV]
+        client_secret = os.environ[TWITCH_CLIENT_SECRET_ENV]
+        data = {
+            "client_id": self.twitch_client_id,
+            "client_secret": client_secret,
+            "grant_type": "client_credentials",
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post("https://id.twitch.tv/oauth2/token", json=data) as response:
+                if 200 <= response.status <= 299:
+                    js = await response.json()
+                    self.twitch_request_headers = {
+                        "Authorization": f"Bearer {js['access_token']}",
+                        "Client-Id": self.twitch_client_id,
+                    }
+                    logging.info("Successfully logged in to twitch")
+                else:
+                    logging.error(f"Could not log in to twitch, http status {response.status}")
+
     @discord.ext.tasks.loop(seconds=POLL_TIME)
     async def poll_twitch(self):
-        await self.announce({"user_name": "me", "user_login": "unit327"})
+        async with aiohttp.ClientSession(headers=self.twitch_request_headers) as session:
+            # Screw pagination, there are never going to be more than 20 people streaming nethack at the same time right?
+            async with session.get(TWITCH_QUERY) as response:
+                if 200 <= response.status <= 299:
+                    js = await response.json()
+                    await self.announce(js["data"])
 
     @poll_twitch.before_loop
     async def wait(self):
-        await self.wait_until_ready()
+        await asyncio.gather(self.wait_until_ready(), self.twitch_auth())
 
-    async def announce(self, stream):
-        message = f"{stream['user_name']} is streaming Nethack!"
-        link = f"https://twitch.tv/{stream['user_login']}"
-
-        logging.info(message)
+    async def announce(self, streams):
         channel = self.get_channel(DISCORD_CHANNEL)
-        await channel.send(f"{message}\n{link}")
+        for st in streams:
+            message = f"{stream['user_name']} is streaming Nethack!"
+            link = f"https://twitch.tv/{stream['user_login']}"
+            logging.info(message)
+            await channel.send(f"{message}\n{link}")
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting")
 
-    announced_streamers = set()
-    #twitch = twitch_session()
     discord_client = DiscordClient(intents=discord.Intents.default())
     discord_client.run(os.environ[DISCORD_BOT_TOKEN_ENV])
-    return
-    
-    while True:
-        try:
-            # Screw pagination, there are never going to be more than 20 people streaming nethack at the same time right?
-            response = twitch.get(f"https://api.twitch.tv/helix/streams?game_id={NETHACK_TWITCH_GAME_ID}&type=live")
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            logging.exception(e)
-            asyncio.sleep(ERROR_RETRY_TIME)
-            if e.status in {401, 403}:
-                twitch = twitch.session()  # too lazy to use refresh token timeout
-                continue
-        except requests.exceptions.ConnectionError:
-            logging.exception(e)
-            asyncio.sleep(ERROR_RETRY_TIME)
-            continue
-
-        current_streamers = set()
-        for stream in response.json()["data"]:
-            stream_key = (stream["user_login"], stream["started_at"])
-            if stream_key not in announced_streamers:
-                try:
-                    announce(stream, discord_client)
-                    current_streamers.add(stream_key)
-                except Exception as e:
-                    logging.exception(e)
-                    #discord_client = discord_session()  # too lazy to use refresh token timeout
-
-        announced_streamers = current_streamers
-        asyncio.sleep(POLL_TIME)
 
 
 if __name__ == "__main__":
