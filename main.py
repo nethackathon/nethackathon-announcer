a5bfb9bb6d53cfae58b2d470e382da6c3f7f240f
Daniel Kinsman <danielkinsman@riseup.net>
2023-09-27 11:10:59 +1000

    working

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 #! /usr/bin/env python
 
 import logging
-import oauthlib.oauth2
 import os
 import requests
-import requests_oauthlib
+import requests.exceptions
+import time
 
 
 CLIENT_ID_ENV = "TWITCH_CLIENT_ID"
 CLIENT_SECRET_ENV = "TWITCH_CLIENT_SECRET"
 NETHACK_TWITCH_GAME_ID = 130
+#NETHACK_TWITCH_GAME_ID = 516575 # valorant for testing
 # get again with this:
 #   response = twitch.get("https://api.twitch.tv/helix/games?name=nethack&name=nethack-1987")
+POLL_TIME = 10
+ERROR_RETRY_TIME = 120
 
 
 def twitch_session():
@@ -36,10 +39,48 @@ def twitch_session():
     return session
 
 
+def announce(stream):
+    message = f"{stream['user_name']} is streaming Nethack!"
+    link = "https://twitch.tv/{stream['user_login']}"
+
+    announce_log(message, link)
+
+
+def announce_log(message, link):
+    logging.info(message)
+
+
 def main():
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Starting")
+    announced_streamers = set()
     twitch = twitch_session()
-    response = twitch.get(f"https://api.twitch.tv/helix/streams?game_id={NETHACK_TWITCH_GAME_ID}&type=live")
-    print(response.json())
+    
+    while True:
+        try:
+            # Screw pagination, there are never going to be more than 20 people streaming nethack at the same time right?
+            response = twitch.get(f"https://api.twitch.tv/helix/streams?game_id={NETHACK_TWITCH_GAME_ID}&type=live")
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            logging.exception(e)
+            time.sleep(ERROR_RETRY_TIME)
+            if e.status in {401, 403}:
+                twitch = twitch.session()  # too lazy to use refresh token timeout
+                continue
+        except requests.exceptions.ConnectionError:
+            logging.exception(e)
+            time.sleep(ERROR_RETRY_TIME)
+            continue
+
+        current_streamers = set()
+        for stream in response.json()["data"]:
+            stream_key = (stream["user_login"], stream["started_at"])
+            current_streamers.add(stream_key)
+            if stream_key not in announced_streamers:
+                announce(stream)
+
+        announced_streamers = current_streamers
+        time.sleep(POLL_TIME)
 
 
 if __name__ == "__main__":
